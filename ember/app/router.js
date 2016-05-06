4fd4d49f01ffb06ba433b858826a984885357470
lilo <lilo@squareup.com>
2016-05-06 11:23:24 -0700

    DS.ActiveModelAdapter not a thing?

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
 	this.route('about');
+  this.resource('speakers', function() {
+    this.route('show', {path: ':speaker_id'});
+  });
 });
 
 export default Router;
